7f44c9fa8b9a966f11c0c31617cb28db928faeeb
sa3eed96 <30784464+sa3eed96@users.noreply.github.com>
2017-12-08 01:01:22 +0200

    Update MainForm.cs

--- ImageEncryptCompress/MainForm.cs
+++ ImageEncryptCompress/MainForm.cs
@@ -37,12 +37,13 @@ namespace ImageQuantization
             int pos = int.Parse(tapText.Text);
             int x = Convert.ToInt32(initialsed, 2);
             int len = initialsed.Length;
-             ImageMatrix = ImageOperations.incrept(ImageMatrix, ref x, len, pos);
-             huffman h = new huffman(ImageMatrix);
-             Dictionary<byte, string> red = h.getRedCode();
-             Dictionary<byte, string> blue = h.getBlueCode();
-             Dictionary<byte, string> green = h.getGreenCode();
-             ImageOperations.DisplayImage(ImageMatrix, pictureBox2);
+            ImageMatrix = ImageOperations.incrept(ImageMatrix, ref x, len, pos);
+            huffman h = new huffman(ImageMatrix);
+            Dictionary<byte, string> red = h.getRedCode();
+            Dictionary<byte, string> blue = h.getBlueCode();
+            Dictionary<byte, string> green = h.getGreenCode();
+        //    h.writeToFile("path here");
+            ImageOperations.DisplayImage(ImageMatrix, pictureBox2);
         }
 
        
